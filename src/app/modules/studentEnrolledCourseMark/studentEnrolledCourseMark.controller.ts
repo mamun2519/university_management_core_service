2a0e7546a9854d3e92572963c830fd067deb2b21
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-09-07 23:42:53 +0600

    add to student enrolled mark api

---
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.controller.ts
@@ -0,0 +1,40 @@
+import { Request, Response } from 'express';
+import httpStatus from 'http-status';
+import catchAsync from '../../../shared/catchAsync';
+import pick from '../../../shared/pick';
+import sendResponse from '../../../shared/sendResponse';
+import { studentEnrolledCourseMarkFilterableFields } from './studentEnrolledCourseMark.constant';
+import { studentEnrollCourseMarkService } from './studentEnrolledCourseMark.service';
+
+const getAllFromDB = catchAsync(async (req: Request, res: Response) => {
+  const filters = pick(req.query, studentEnrolledCourseMarkFilterableFields);
+  const options = pick(req.query, ['limit', 'page', 'sortBy', 'sortOrder']);
+  const result = await studentEnrollCourseMarkService.getAllFromDB(
+    filters,
+    options
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'Student course marks fetched successfully',
+    meta: result.meta,
+    data: result.data,
+  });
+});
+
+const updateStudentMarks = catchAsync(async (req: Request, res: Response) => {
+  const result = await studentEnrollCourseMarkService.updateStudentMarks(
+    req.body
+  );
+  sendResponse(res, {
+    statusCode: httpStatus.OK,
+    success: true,
+    message: 'marks updated!',
+    data: result,
+  });
+});
+
+export const StudentEnrolledCourseMarkConroller = {
+  getAllFromDB,
+  updateStudentMarks,
+};
--- src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.service.ts
+++ src/app/modules/studentEnrolledCourseMark/studentEnrolledCourseMark.service.ts
@@ -1,8 +1,17 @@
-import { ExamType, PrismaClient } from '@prisma/client';
+import {
+  ExamType,
+  PrismaClient,
+  StudentEnrolledCourseMark,
+} from '@prisma/client';
 import {
   DefaultArgs,
   PrismaClientOptions,
 } from '@prisma/client/runtime/library';
+import { paginationHelpers } from '../../../helpers/paginationHelper';
+import { IGenericResponse } from '../../../interfaces/common';
+import { IPaginationOptions } from '../../../interfaces/pagination';
+import prisma from '../../../prisma/prisma';
+import { IStudentEnrolledCourseMarkFilterRequest } from './studentEnrolledCourseMark.interface';
 
 const createStudentEnrolledDefaultMark = async (
   prismaClient: Omit<
@@ -92,6 +101,52 @@ const createStudentEnrolledDefaultMark = async (
   }
 };
 
+const getAllFromDB = async (
+  filters: IStudentEnrolledCourseMarkFilterRequest,
+  options: IPaginationOptions
+): Promise<IGenericResponse<StudentEnrolledCourseMark[]>> => {
+  const { limit, page } = paginationHelpers.calculatePagination(options);
+
+  const marks = await prisma.studentEnrolledCourseMark.findMany({
+    where: {
+      student: {
+        id: filters.studentId,
+      },
+      academicSemester: {
+        id: filters.academicSemesterId,
+      },
+      studentEnrollCourse: {
+        course: {
+          id: filters.courseId,
+        },
+      },
+    },
+    include: {
+      studentEnrollCourse: {
+        include: {
+          course: true,
+        },
+      },
+      student: true,
+    },
+  });
+
+  return {
+    meta: {
+      total: marks.length,
+      page,
+      limit,
+    },
+    data: marks,
+  };
+};
+
+const updateStudentMarks = async (payload: any) => {
+  console.log(payload);
+};
+
 export const studentEnrollCourseMarkService = {
   createStudentEnrolledDefaultMark,
+  getAllFromDB,
+  updateStudentMarks,
 };
